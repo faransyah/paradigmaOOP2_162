e61918e9d6df6b71b03d6a0b27e55476e628874c
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-05-29 14:58:50 +0700

    class manusia

--- multipewarisan2.cpp
+++ multipewarisan2.cpp
@@ -2,4 +2,19 @@
 #include <string>
 using namespace std;
 
-class orang 
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+};
+
+class manusia {
+
+};
